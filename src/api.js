e43509beec010c0ed6a9017c01a9b77ba79b7d3a
noamit <noa1amit2@gmail.com>
2024-08-09 14:46:44 +0300

    api for register,login + get,post,delete expenses

--- src/api.js
+++ src/api.js
@@ -3,12 +3,12 @@ const BASE_URL = "http://localhost:5000";
 
 export async function register(username, password) {
   const postRequest = { username: username, password: password };
-  return await axios.post(BASE_URL + "/auth/register", postRequest);
+  return await axios.post(BASE_URL + "/register", postRequest);
 }
 
 export async function login(username, password) {
   const postRequest = { username: username, password: password };
-  return await axios.post(BASE_URL + "/auth/login", postRequest);
+  return await axios.post(BASE_URL + "/login", postRequest);
 }
 
 export async function home(token) {
@@ -18,5 +18,34 @@ export async function home(token) {
     },
   });
 
+  return response.data;
+}
+
+export async function insert_expense(token, name, amount, date) {
+  const postRequest = { name: name, amount: amount, date: date };
+  console.log(date);
+  const response = await axios.post(BASE_URL + "/expense", postRequest, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+}
+
+export async function get_expenses(token) {
+  const response = await axios.get(BASE_URL + "/expense", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+}
+
+export async function delete_expenses(token, id) {
+  const response = await axios.delete(BASE_URL + "/expense/" + id, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
   return response.data;
 }
