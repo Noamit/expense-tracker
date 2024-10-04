b44f6d7058b717cb42831b4044df395a9bf4dbc5
noamit <noa1amit2@gmail.com>
2024-10-04 17:17:37 +0300

    show langs & translate pages for admins on navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,8 +2,12 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "./css/navbar.css";
+import { isAdmin } from "./auth"; // Path to the isAdmin function
 
 function NavBar() {
+  const accessToken = localStorage.getItem("access_token");
+  const refreshToken = localStorage.getItem("refresh_token");
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
@@ -11,6 +15,10 @@ function NavBar() {
         <Nav>
           <Nav.Link href="/">Home</Nav.Link>
           <Nav.Link href="/category">Categories</Nav.Link>
+          {isAdmin(accessToken) && <Nav.Link href="/langs">Langs</Nav.Link>}
+          {isAdmin(accessToken) && (
+            <Nav.Link href="/translates">Translates</Nav.Link>
+          )}
           {/* <Nav.Link href="/study">Study Area</Nav.Link>
           <Nav.Link href="/games">Games Area</Nav.Link> */}
         </Nav>
