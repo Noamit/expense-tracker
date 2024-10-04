e3c285f5bb42f2c4df5f9366ec97d80050b4394f
noamit <noa1amit2@gmail.com>
2024-10-04 17:16:43 +0300

    add translate page for show all translate, and insert and update translates' pages

---
+++ src/components/Translate.js
@@ -0,0 +1,163 @@
+import React, { useState, useEffect } from "react";
+import { useParams, useLocation, useNavigate } from "react-router-dom";
+import {
+  Button,
+  TextField,
+  MenuItem,
+  Container,
+  Box,
+  Typography,
+} from "@mui/material";
+import { get_translate, update_translate } from "../api";
+import NavBar from "../Navbar";
+
+function Translate({ setAccessToken, setRefreshToken }) {
+  const accessToken = localStorage.getItem("access_token");
+  const refreshToken = localStorage.getItem("refresh_token");
+
+  const { id } = useParams();
+  const [translate, setTranslate] = useState({});
+  const [updatedTranslate, setUpdatedTranslate] = useState({});
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const { langs } = location.state || {};
+
+  useEffect(() => {
+    try {
+      get_translate(
+        accessToken,
+        setAccessToken,
+        setRefreshToken,
+        navigate,
+        id
+      ).then((value) => {
+        setTranslate(value);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }, [id]);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setTranslate({ ...translate, [name]: value });
+    setUpdatedTranslate({ ...updatedTranslate, [name]: value });
+  };
+
+  const handleLangChange = (e) => {
+    const LangId = e.target.value;
+    setTranslate({ ...translate, lang_id: LangId });
+    setUpdatedTranslate({ ...updatedTranslate, lang_id: LangId });
+  };
+
+  const handleSubmit = async (e) => {
+    try {
+      const updateData = {};
+      Object.keys(updatedTranslate).forEach((key) => {
+        updateData[key] = updatedTranslate[key];
+      });
+      await update_translate(
+        accessToken,
+        setAccessToken,
+        setRefreshToken,
+        navigate,
+        id,
+        updateData
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  if (!translate) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+      <NavBar />
+      <Container maxWidth="sm">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Edit Translate
+          </Typography>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSubmit();
+            }}
+            style={{ width: "100%", marginTop: "20px" }}
+          >
+            <TextField
+              fullWidth
+              margin="normal"
+              id="expense-name"
+              label="Translate Name"
+              variant="outlined"
+              name="key"
+              InputLabelProps={{
+                shrink: translate.key !== "" && translate.key !== null, // Force label to shrink when there's a value
+              }}
+              value={translate.key}
+              InputProps={{
+                readOnly: true, // Makes the field read-only
+              }}
+              required
+            />
+
+            <TextField
+              fullWidth
+              margin="normal"
+              id="expense-description"
+              label="Translate Value"
+              variant="outlined"
+              name="value"
+              value={translate.value}
+              InputLabelProps={{
+                shrink: translate.value !== "" && translate.value !== null, // Force label to shrink when there's a value
+              }}
+              onChange={handleInputChange}
+              required
+            />
+
+            <TextField
+              fullWidth
+              select
+              margin="normal"
+              name="lang_id"
+              label="Select Language"
+              value={translate.lang_id || ""}
+              onChange={handleLangChange}
+              required
+            >
+              {langs.map((lang) => (
+                <MenuItem key={lang.id} value={lang.id}>
+                  {lang.name}
+                </MenuItem>
+              ))}
+            </TextField>
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ marginTop: "20px" }}
+            >
+              Save Changes
+            </Button>
+          </form>
+        </Box>
+      </Container>
+    </>
+  );
+}
+
+export default Translate;
---
+++ src/components/Translates.js
@@ -0,0 +1,121 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { get_translates, delete_translate, get_langs } from "../api";
+import "../css/home.css";
+
+import NavBar from "../Navbar";
+
+function Translates({ setAccessToken, setRefreshToken }) {
+  const accessToken = localStorage.getItem("access_token");
+  const refreshToken = localStorage.getItem("refresh_token");
+
+  const [translates, setTranslates] = useState([]);
+  const [langs, setLangs] = useState([]);
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    try {
+      get_translates(
+        accessToken,
+        setAccessToken,
+        setRefreshToken,
+        navigate
+      ).then((value) => {
+        setTranslates(value);
+      });
+      get_langs(accessToken, setAccessToken, setRefreshToken, navigate).then(
+        (value) => {
+          setLangs(value);
+        }
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  const handle_delete = async (id) => {
+    try {
+      await delete_translate(
+        accessToken,
+        setAccessToken,
+        setRefreshToken,
+        navigate,
+        id
+      );
+      const updatedTranslates = translates.filter(
+        (translate) => translate.id !== id
+      );
+      setTranslates(updatedTranslates);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <NavBar />
+
+      <div className="container">
+        <div>
+          <button
+            id="create-expense"
+            type="button"
+            onClick={() =>
+              navigate(`/translate`, {
+                state: { langs },
+              })
+            }
+          >
+            Add Translate
+          </button>
+        </div>
+        <div className="expense-table">
+          <table>
+            <thead>
+              <tr>
+                <th>Translate Key</th>
+                <th>Translate Value</th>
+                <th>Language</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody id="expense-list">
+              {translates.map((translate, index) => (
+                <tr key={translate.id}>
+                  <td>{translate.key}</td>
+                  <td>{translate.value}</td>
+                  <td>{translate.lang ? translate.lang.name : ""}</td>
+                  <td>
+                    <button
+                      id="edit-expense"
+                      type="button"
+                      onClick={() =>
+                        navigate(`/translate/${translate.id}`, {
+                          state: { langs },
+                        })
+                      }
+                    >
+                      Edit
+                    </button>
+                    <button
+                      id="delete-expense"
+                      type="submit"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        handle_delete(translate.id);
+                      }}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Translates;
